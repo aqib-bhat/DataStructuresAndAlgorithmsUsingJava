621c798c04dbfb1536f03dff5c8e7803f2df03b3
Aqib Niaz Bhat <aqib.n.bhat@gmail.com>
2018-09-23 02:08:33 -0700

    Addeing the full implementation

--- src/org/dsandalgos/misc/FibonacciSeriesUsingRecursion.java
+++ src/org/dsandalgos/misc/FibonacciSeriesUsingRecursion.java
@@ -2,6 +2,20 @@ package org.dsandalgos.misc;
 
 public class FibonacciSeriesUsingRecursion {
 	public static void main(String args[]) {
-		
+		FibonacciSeriesUsingRecursion fibonacciSeriesUsingRecursion = new FibonacciSeriesUsingRecursion();
+		System.out.println("The first 10 numbers in the Fibonacci series are: ");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(fibonacciSeriesUsingRecursion.fibonnaci(i) + ", ");
+		}
+	}
+
+	private int fibonnaci(int n) {
+		if (n == 0) {
+			return 0;
+		} else if (n == 1){
+			return 1;
+		} else {
+			return fibonnaci(n -1) + fibonnaci (n - 2);
+		}
 	}
 }
