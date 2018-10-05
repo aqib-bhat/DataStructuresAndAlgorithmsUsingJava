15d1709c3db55ad38c1fa618fe91472334d21b0f
Aqib Niaz Bhat <aqib.n.bhat@gmail.com>
2018-10-04 18:06:43 -0700

    Added defensive coding for non-sequential calls

--- src/org/dsandalgos/dynamicprogramming/FibonacciSeriesUsingDP.java
+++ src/org/dsandalgos/dynamicprogramming/FibonacciSeriesUsingDP.java
@@ -5,14 +5,21 @@ import java.util.*;
 public class FibonacciSeriesUsingDP {
 	public static void main(String args[]) {
 		FibonacciSeriesUsingDP fibonacciSeriesUsingDP = new FibonacciSeriesUsingDP();
+		System.out.println("The 7th number in the Fibonacci series is: ");
+		System.out.println(fibonacciSeriesUsingDP.fibUsingMemo(7));
 		System.out.println("The first 10 numbers in the Fibonacci series are: ");
 		for (int i = 0; i < 10; i++) {
-			System.out.print(fibonacciSeriesUsingDP.memoFib(i) + ", ");
+			System.out.print(fibonacciSeriesUsingDP.fibUsingMemo(i) + ", ");
 		}
 	}
 
 	// Map (memo) for storing the computed values.
 	private HashMap<Integer, Integer> fibonacciMemo = new HashMap<>();
+	
+	FibonacciSeriesUsingDP() {
+	    fibonacciMemo.put(0,0);
+	    fibonacciMemo.put(1,1);
+	}
 
 	/*
 	 * Method for calculating the Fibonacci series numbers following a
@@ -20,16 +27,21 @@ public class FibonacciSeriesUsingDP {
 	 * @param n The position for which we want to calculate the value in the Fibonacci series.
 	 * @return The value at the given position in the Fibonacci series.
 	*/	
-	private int memoFib (int n) {
-		int fib;
-		if (n == 0) {
-			fib = 0;
-		} else if (n < 2) {
-			fib = 1;
+	private int fibUsingMemo (int n) {
+		int fib = 0;
+		
+		if (n < 2) {
+			fib = n;
 		} else {
-			fib = fibonacciMemo.get(n-1) + fibonacciMemo.get(n-2);
+			if (fibonacciMemo.containsKey(n-1)) {
+				fib += fibonacciMemo.get(n-1);
+			} else {
+				// Defensive coding, in case this method was not called sequentially for arg from 0 to n-1.
+				fib += fibUsingMemo(n-1);
+			}
+			fib += fibonacciMemo.get(n-2);
+			fibonacciMemo.put(n, fib);
 		}
-		fibonacciMemo.put(n, fib);
 		return fib;
 	}
 }
