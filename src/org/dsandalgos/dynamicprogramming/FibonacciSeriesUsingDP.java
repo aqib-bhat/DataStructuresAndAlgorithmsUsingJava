dac16301064c7992c3487575aeafbf9fae54087d
Aqib Niaz Bhat <aqib.n.bhat@gmail.com>
2018-10-05 21:28:36 -0700

    Added the bottom-up approach

--- src/org/dsandalgos/dynamicprogramming/FibonacciSeriesUsingDP.java
+++ src/org/dsandalgos/dynamicprogramming/FibonacciSeriesUsingDP.java
@@ -7,13 +7,17 @@ public class FibonacciSeriesUsingDP {
 		FibonacciSeriesUsingDP fibonacciSeriesUsingDP = new FibonacciSeriesUsingDP();
 		System.out.println("The 7th number in the Fibonacci series is: ");
 		System.out.println(fibonacciSeriesUsingDP.fibUsingMemo(7));
-		System.out.println("The first 10 numbers in the Fibonacci series are: ");
+		System.out.println("\nThe first 10 numbers in the Fibonacci series are: ");
 		for (int i = 0; i < 10; i++) {
 			System.out.print(fibonacciSeriesUsingDP.fibUsingMemo(i) + ", ");
 		}
+		System.out.println("\n\nNow using the bottom-up approach...");
+		System.out.println("The first 15 numbers in the Fibonacci series are: ");
+		fibonacciSeriesUsingDP.fibUsingBottomUpMemo(15).entrySet()
+		    .forEach(e -> System.out.print(e.getValue() + ", "));
 	}
 
-	// Map (memo) for storing the computed values.
+	// Map (memo) for storing the computed values (top-down approach).
 	private HashMap<Integer, Integer> fibonacciMemo = new HashMap<>();
 	
 	FibonacciSeriesUsingDP() {
@@ -43,5 +47,23 @@ public class FibonacciSeriesUsingDP {
 			fibonacciMemo.put(n, fib);
 		}
 		return fib;
+	}
+
+	/*
+	 * Method for calculating the Fibonacci series numbers following a
+	 * bottom-up approach.
+	 * @param n The position up to which we want to calculate the values in the Fibonacci series.
+	 * @return A map containing the values of the first n numbers in the Fibonacci series.
+	*/		
+	private HashMap<Integer, Integer> fibUsingBottomUpMemo(int n) {
+		HashMap<Integer, Integer> fibBottomUpMemo = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			if (i < 2) {
+				fibBottomUpMemo.put(i, i);
+			} else {
+				fibBottomUpMemo.put(i, fibBottomUpMemo.get(i - 1) + fibBottomUpMemo.get(i - 2));
+			}
+		}
+		return fibBottomUpMemo;
 	}
 }
